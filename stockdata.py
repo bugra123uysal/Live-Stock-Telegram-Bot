6b624b9545d9b22bc4dadcb3767954a55baf127e
buğra uysal <uysalbugra134@gmail.com>
2025-06-22 01:01:17 +0300

    I installed a basic level alarm system

--- stockdata.py
+++ stockdata.py
@@ -1,20 +1,37 @@
 import requests 
 import pandas as pd
+import time
 
-api_key="cp7rd3pr01qi8q89arpgcp7rd3pr01qi8q89arq0"
+api_key="******"
 
 stocks=["AAPL", "MSFT", "GOOGL","TSLA","TSM","AMZN"]
+#alarm target
+alarm={
+    "AAPL":220,
+    "MSFT":460
 
-ecl=[]
-for stock in stocks:
-    url=f"https://finnhub.io/api/v1/quote?symbol={stock}&token={api_key}"
-    get=requests.get(url).json()
+}
 
-    if 'c' in get:
-        price=get['c'] #current prices
-        print(f"{stock}: ${price:.2f}")
+
+while True:
+
+   for stock  in stocks:
+       url=f"https://finnhub.io/api/v1/quote?symbol={stock}&token={api_key}"
+       get=requests.get(url).json()
+   
+       if 'c' in get:
+           price=get['c'] #current prices
+           print(f"{stock}: ${price:.2f}")
+          
        
-    
-    else:
-        print(f"{stock}: tekar deneyiniz")
+       else:
+           print(f"{stock}: tekar deneyiniz")
 
+    
+         #alarm
+       if stock in alarm and price >= alarm[stock]:
+           print(f"{stock}: fiyat seviyesine ulaşdı {price} >= {alarm[stock]}   ")
+       else:
+           print(f"{stock} fiyat seviyesine ulaşmadı ")
+  
+   time.sleep(10)
