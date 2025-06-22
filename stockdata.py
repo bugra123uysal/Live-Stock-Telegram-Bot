095ab3f9234e8001c21510aea2fa53114c3b8985
buğra uysal <uysalbugra134@gmail.com>
2025-06-22 23:38:09 +0300

    telegram bot added and working

--- stockdata.py
+++ stockdata.py
@@ -2,13 +2,23 @@ import requests
 import pandas as pd
 import time
 
-api_key="******"
+def telegrambot(note):
+       bot_token="****"
+       chat_id="****"
+       url=f"https://api.telegram.org/bot{ bot_token}/sendMessage"
+       data={"chat_id":chat_id, "text": note }
+       response= requests.post(url, data=data)
+       
+
+
+api_key="****"
 
 stocks=["AAPL", "MSFT", "GOOGL","TSLA","TSM","AMZN"]
 #alarm target
 alarm={
-    "AAPL":220,
-    "MSFT":460
+    "AAPL":201,
+    "MSFT":460,
+    "TSLA":322.16
 
 }
 
@@ -20,18 +30,21 @@ while True:
        get=requests.get(url).json()
    
        if 'c' in get:
-           price=get['c'] #current prices
-           print(f"{stock}: ${price:.2f}")
-          
-       
-       else:
-           print(f"{stock}: tekar deneyiniz")
-
+            price=get['c'] #current prices
+            print(f"{stock}: ${price:.2f}")
     
-         #alarm
-       if stock in alarm and price >= alarm[stock]:
-           print(f"{stock}: fiyat seviyesine ulaşdı {price} >= {alarm[stock]}   ")
+               #alarm
+            if stock in alarm and price >= alarm[stock]:
+              print(f"{stock}: fiyat seviyesine ulaşdı {price} >= {alarm[stock]}  GÜNCEL FİYATLAR: {stock}: ${price:.2f}  ")
+              note=f" {stock}: fiyat seviyesine ulaşdı {price} >= {alarm[stock]    }  " 
+             
+              telegrambot(note)
+            else:
+               print(f"{stock} fiyat seviyesine ulaşmadı ")
        else:
-           print(f"{stock} fiyat seviyesine ulaşmadı ")
+         print(f"{stock}: tekar deneyiniz")
   
    time.sleep(10)
+
+   
+    
