b12717171000a6068317c58f165f25777efa0946
buğra uysal <uysalbugra134@gmail.com>
2025-07-01 00:54:06 +0300

    added help command for telegram bot

--- stockdata.py
+++ stockdata.py
@@ -7,15 +7,15 @@ from telegram.ext import Updater, CommandHandler
 
 
 def telegrambot(note):
-       bot_token="****"# bot token
-       chat_id="*****" # chat ıd 
+       bot_token="**"
+       chat_id="****"
        url=f"https://api.telegram.org/bot{bot_token}/sendMessage"
        data={"chat_id":chat_id, "text": note }
-       requests.post(url, data=data) #mesajı gönderir
+       requests.post(url, data=data)
        
 
 
-api_key="*****" # finnhub api key 
+api_key="****"
 
 stocks=["AAPL", "MSFT", "GOOGL","TSLA","TSM","AMZN","PM","META","NFLX","BABA","BAC","JNJ","WMT","BTI"]
 #alarm target
@@ -25,10 +25,25 @@ alarm={
     "GOOGL":170.68
 
 }
-# telegram komut 
+
+def helpp(update , context):
+        try:
+          
+          
+          if helpp:  
+
+           yardım=("- for helping  : / help   - for  prices  /fiyatt TSM  ")    
+           update.message.reply_text(yardım)
+          else:
+              update.message.reply_text
+           
+        except:
+            update.message.reply_text
+
+# telegram fiyat komut                
 def fiyatt(update, context):     
        try:
-          hisse=context.args[0].upper() # komuttan hisse adını alınır
+          hisse=context.args[0].upper()
           url=f"https://finnhub.io/api/v1/quote?symbol={hisse}&token={api_key}"
           rege=requests.get(url).json()
           fiyatt=rege.get("c")
@@ -42,7 +57,7 @@ def fiyatt(update, context):
           update.message.reply_text(note)
    
        except:
-           update.message.reply_text("kullanım /fiyat")
+           update.message.reply_text("kullanım /fiyatt")
 
 
 while True:
@@ -86,16 +101,22 @@ while True:
    ecl.to_excel("abd_hisse.xlsx", index=True )  
    telegrambot(note)
    if __name__ == "__main__":
-       bot_token="*****"
+       bot_token="8151142897:AAGoXgaHGKXZ4gKp2emt2KJ_Kx6dTBs5SJs"
        #Telegram bot'unu başlatır ve gelen telegram mesajlarını alıcak updater i tanımlar
        updater=Updater(bot_token, use_context=True)
+       ater=Updater(bot_token, use_context=True)
        dp=updater.dispatcher 
+       aa=updater.dispatcher
+       # hisse fiyatı komitti
        dp.add_handler(CommandHandler("fiyatt", fiyatt))
+       #bilgilendirme
+       aa.add_handler(CommandHandler ("helpp", helpp ))
+
        updater.start_polling()
+       ater.idle()
+
+
 
        while True:
             fiyatt()
             time.sleep(3000)
-           
-
- 
